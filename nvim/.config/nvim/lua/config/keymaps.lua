f0ea9471a2bb991ac7e306650f997de9bec666e2
Samuel MICHAUX - OlProG <samuel.michaux@ik.me>
2024-08-19 09:12:40 +0200

    add minimap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,5 +1,6 @@
 local map = LazyVim.safe_keymap_set
---
+local default_opts = { noremap = true, silent = true }
+
 -- map("n", "<leader>w-", "<C-W>s", { desc = "Split Window Below", remap = true })
 -- map("n", "<leader>w$", "<C-W>v", { desc = "Split Window Right", remap = true })
 -- map("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
@@ -70,3 +71,50 @@ map("n", "<leader>os", "<cmd>ObsidianSearch<CR>", { desc = "Search Obsidian" })
 map("n", "<leader>oq", "<cmd>ObsidianQuickSwitch<CR>", { desc = "Quick Switch" })
 
 map("", "<leader>ll", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+
+-- MiniSurround (réassigné sous <m>)
+map("n", "<m-sr>", '<cmd>lua require("mini.surround").replace()<CR>', default_opts)
+map("n", "<m-sF>", '<cmd>lua require("mini.surround").find_left()<CR>', default_opts)
+map("n", "<m-sh>", '<cmd>lua require("mini.surround").highlight()<CR>', default_opts)
+map("n", "<m-sf>", '<cmd>lua require("mini.surround").find_right()<CR>', default_opts)
+map("n", "<m-sd>", '<cmd>lua require("mini.surround").delete()<CR>', default_opts)
+map("n", "<m-sa>", '<cmd>lua require("mini.surround").add()<CR>', default_opts)
+map("n", "<m-sn>", '<cmd>lua require("mini.surround").update_n_lines()<CR>', default_opts)
+
+-- Commentaires
+map("n", "<leader>/", '<cmd>lua require("Comment.api").toggle.linewise.current()<CR>', default_opts)
+map("n", "<leader>co", '<cmd>lua require("Comment.api").insert.linewise.above()<CR>', default_opts)
+map("n", "<leader>cO", '<cmd>lua require("Comment.api").insert.linewise.below()<CR>', default_opts)
+
+-- LSP
+map("n", "<leader>ll", '<cmd>lua require("lsp_lines").toggle()<CR>', default_opts)
+map("n", "<leader>lg", "<cmd>LazyGit<CR>", default_opts)
+
+-- Gestion des fenêtres
+map("n", "<leader>wD", "<cmd>lua vim.diagnostic.open_float()<CR>", default_opts)
+map("n", "<leader>wS", '<cmd>lua require("hydra").window_hydra()<CR>', default_opts)
+map("n", "<leader>wm", "<cmd>MaximizerToggle<CR>", default_opts)
+
+-- Git
+map("n", "<leader>gg", "<cmd>Git<CR>", default_opts)
+map("n", "<leader>gl", "<cmd>LazyGit<CR>", default_opts)
+map("n", "<leader>gb", "<cmd>Git blame<CR>", default_opts)
+map("n", "<leader>gs", "<cmd>Git status<CR>", default_opts)
+map("n", "<leader>gc", "<cmd>Git commit<CR>", default_opts)
+
+-- Terminal
+map("n", "<leader>tt", "<cmd>terminal<CR>", default_opts)
+map("n", "<leader>tl", '<cmd>lua require("myplugin").run_last()<CR>', default_opts)
+
+-- Fichiers
+map("n", "<leader>fo", "<cmd>Neotree toggle<CR>", default_opts)
+map("n", "<leader>ff", "<cmd>Telescope find_files<CR>", default_opts)
+map("n", "<leader>fr", "<cmd>Telescope oldfiles<CR>", default_opts)
+
+-- Mode visuel
+map("x", "<leader>a", '<cmd>lua require("surround").surround_add()<CR>', default_opts)
+map("x", "<leader>i", '<cmd>lua require("surround").surround_add()<CR>', default_opts)
+
+-- Mode opérateur
+map("o", "<leader>a", '<cmd>lua require("surround").surround_add()<CR>', default_opts)
+map("o", "<leader>i", '<cmd>lua require("surround").surround_add()<CR>', default_opts)
